2674cacb4e0c138985276e5b7aaba5241d54ca29
Steve Roth <steve@sidestudios.com>
2017-03-23 14:49:19 -0700

    Initial commit from bazaar example

---
+++ README.md
@@ -0,0 +1,4 @@
+Python BV SEO SDK using Django
+========
+
+Copied from https://github.com/bazaarvoice/HostedUIResources/tree/master/SEOIntegration/examples/python/django
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="django-bazaarvoice",
+    version="0.1",
+    url="https://github.com/SideStudios/django-bazaarvoice",
+    packages=["bvseosdk"],
+    license="",
+    description="Django integration for BazaarVoice SEO",
+    long_description=open("README.md").read(),
+)
